789360a364f662569aa2f8526afbfef8e4b3104c
Caonabo Mena <cmena7512@gmail.com>
2022-11-29 20:44:11 -0400

    Modulo para la gestion de la empresa JGAcueductos

---
+++ addons/JGAcueducto/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': "JG Acueductos",
+    'summary': """
+        Incorpora funcionalidades para que jg acueductos funcione
+        """,
+    'author': "Caonabo Mena",
+    'license': 'LGPL-3',
+    'category': 'Generic Modules',
+    'version': '15.0.1.0.0',
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    # any module necessary for this one to work correctly
+    'depends': ['stock'],
+
+    # always loaded
+    'data': [
+
+    ],
+
+}
---
+++ addons/JGAcueducto/models/Area.py
@@ -0,0 +1,27 @@
+from odoo import models, fields
+
+class StockArea(models.Model):
+    _name="stock.area"
+
+    code = fields.Char("Codigo")
+    category_id = fields.Many2one(comodel_name='product.category', string='Categoria', required=True)
+    name = fields.Char(string='Nombre', required=True)
+    incremental = fields.Integer(string='Incremental', default=1, required=True)
+    mida_secuence = fields.Integer(string='Mida de secuencia', help='número de 0 que se colocara. A la de izquierda', required=True)
+    next_number = fields.Integer(string='Próximo número', help='número inicial en que se empezara a contabilizar', default=0, required=True)
+    cost_percentage = fields.Float(string='Porcentaje de venta sobre el costo', required=True)
+    
+    
+    def GenerateSecuence(self):
+        secuence = self.category_id.name + self.code + str(self.next_number).zfill(self.mida_secuence)
+        self.next_number += self.incremental
+        return secuence
+    
+    _sql_constraints = [
+        ('code_uk_field', 'unique(code)', 'Codigo ya esta registrado en el sistema.')
+    ]
+    
+    
+    
+
+    
